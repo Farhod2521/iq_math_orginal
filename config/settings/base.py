6f16457545ccf70460cc2e52acd5ddb0f53565a8
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-03-15 14:50:38 +0500

    fixed

--- config/settings/base.py
+++ config/settings/base.py
@@ -105,7 +105,11 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'app_user.User'
