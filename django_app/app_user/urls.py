d308cefbabf0fba7ee55c58aea1dd4612584f872
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-03-18 17:34:43 +0500

    fixed

--- django_app/app_user/urls.py
+++ django_app/app_user/urls.py
@@ -8,7 +8,7 @@ from .views import (
 )
 urlpatterns = [
     path('register/', RegisterStudentAPIView.as_view(), name='register_student'),
-    path('verify-sms/', VerifySmsCodeAPIView.as_view(), name='verify_sms'),#register
+    path('register-verify-sms/', VerifySmsCodeAPIView.as_view(), name='verify_sms'),#register
     path('login/', LoginAPIView.as_view(), name='login'),
     path('profile/', StudentProfileAPIView.as_view(), name='profile'),
     path('student_list/', StudentsListView.as_view(), name='student_list'),
