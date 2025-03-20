e1b8b3c279eb7f964a241fdcbb13f313ab4e4d21
Farhod2521 <80619246+Farhod2521@users.noreply.github.com>
2025-03-20 22:53:10 +0500

    fixed

--- django_app/app_teacher/admin.py
+++ django_app/app_teacher/admin.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
 from modeltranslation.admin import TranslationAdmin
 
-from .models import Subject, Topic, Question
+from .models import Subject, Topic, Question, Chapter, Subject_Category
 
-
+@admin.site.register(Subject_Category)
 @admin.register(Subject)
 class SubjectAdmin(TranslationAdmin):
     list_display = ('name', )
 
+
+@admin.register(Chapter)
+class ChapterAdmin(TranslationAdmin):
+    list_display = ('name', )
 
 @admin.register(Topic)
 class TopicAdmin(TranslationAdmin):
