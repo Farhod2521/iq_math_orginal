52e541c5d16d850afa004327a4cac1aa8dbe6e56
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-03-24 10:16:29 +0500

    fixed

--- django_app/app_student/urls.py
+++ django_app/app_student/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import MySubjectsView
+from .views import MySubjectsView, GenerateTestAPIView
 
 urlpatterns = [
     path('my-subjects/', MySubjectsView.as_view(), name='my-subjects'),
+    path('my-generate-test/', GenerateTestAPIView.as_view(), name='generate-test'),
 ]
