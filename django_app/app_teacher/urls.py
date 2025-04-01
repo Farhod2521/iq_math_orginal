4d9b3e98058e7b08d1534f8e86740c1a7f50adbd
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-04-01 09:26:22 +0500

    fixed

--- django_app/app_teacher/urls.py
+++ django_app/app_teacher/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('my-chapters/create/', MyChapterAddCreateView.as_view(), name='chapter-create'),
     path('my-topics/create/', MyTopicAddCreateView.as_view(), name='topic-create'),
     path('my-questions/create/', QuestionAddCreateView.as_view(), name='question-create'),
-    path('my-chapters/list/', MyChapterListView.as_view(), name='my-chapters'),
+    path('my-chapters/list/<int:id>/', MyChapterListView.as_view(), name='my-chapters'),
     path('my-topics/list/<int:id>/', MyTopicListView.as_view(), name='my-topics'),
     path('my-questions/<int:id>/', MyQuestionListView.as_view(), name='my-questions'),
 
