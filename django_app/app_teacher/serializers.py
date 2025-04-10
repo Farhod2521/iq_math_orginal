4670123edd51545e62fc52e518723f91e9146e78
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-04-10 11:29:54 +0500

    fixed

--- django_app/app_teacher/serializers.py
+++ django_app/app_teacher/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import (
-    Subject, Chapter, Topic, Question,
+    Subject, Chapter, Topic, Question,Choice, CompositeSubQuestion
     
 )
 
@@ -25,3 +25,35 @@ class MyTopicAddSerializer(serializers.ModelSerializer):
         model =  Topic
         fields = "__all__"
 
+class ChoiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Choice
+        fields = ['letter', 'text', 'image', 'is_correct']
+
+
+class CompositeSubQuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CompositeSubQuestion
+        fields = ['text', 'correct_answer']
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    choices = ChoiceSerializer(many=True, required=False)
+    sub_questions = CompositeSubQuestionSerializer(many=True, required=False)
+
+    class Meta:
+        model = Question
+        fields = ['id', 'topic', 'question_text', 'question_type', 'level', 'correct_text_answer', 'choices', 'sub_questions']
+
+    def create(self, validated_data):
+        choices_data = validated_data.pop('choices', [])
+        sub_questions_data = validated_data.pop('sub_questions', [])
+        question = Question.objects.create(**validated_data)
+
+        for choice_data in choices_data:
+            Choice.objects.create(question=question, **choice_data)
+
+        for sub_question_data in sub_questions_data:
+            CompositeSubQuestion.objects.create(question=question, **sub_question_data)
+
+        return question
