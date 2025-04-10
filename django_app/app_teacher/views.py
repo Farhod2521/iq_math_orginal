926fb993373a387df44cddd4e37830f8bde6a3da
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-04-10 12:28:39 +0500

    fixed

--- django_app/app_teacher/views.py
+++ django_app/app_teacher/views.py
@@ -72,39 +72,39 @@ class QuestionAddCreateView(APIView):
 
     def post(self, request):
         user = request.user
-
-        # Asosiy savolni serialize qilamiz
         serializer = QuestionSerializer(data=request.data)
-
         if serializer.is_valid():
             try:
                 with transaction.atomic():
                     question = serializer.save()
 
-                    # Savol turi 'choice' yoki 'image_choice' bo'lsa
+                    # Choice savollarini saqlash
                     if question.question_type in ['choice', 'image_choice']:
                         choices_data = request.data.get('choices', [])
-                        seen_choices = set()  # Takrorlanadigan variantlarni tekshirish
+                        seen_choices = set()  
                         for choice_data in choices_data:
-                            # Agar choice ma'lumoti oldin qo'shilgan bo'lsa, o'tkazib yuborilsin
                             choice_tuple = (choice_data['letter'], choice_data['text'], choice_data['is_correct'])
-
+                            # Agar choice takrorlansa, u holda uni o'rnatmaslik
                             if choice_tuple in seen_choices:
                                 continue
                             seen_choices.add(choice_tuple)
-
-                            # Qo'shilgan variantni saqlaymiz
                             choice_data['question'] = question.id
                             choice_serializer = ChoiceSerializer(data=choice_data)
                             if choice_serializer.is_valid():
                                 choice_serializer.save()
                             else:
                                 raise ValueError("Variantdagi ma'lumotlar noto‘g‘ri")
-
-                    # Boshqa turdagi savollarni qo‘shish
+                    
+                    # Composite savollarini saqlash
                     elif question.question_type == 'composite':
                         sub_questions_data = request.data.get('sub_questions', [])
+                        seen_sub_questions = set()  
                         for sub_data in sub_questions_data:
+                            sub_tuple = (sub_data['text1'], sub_data['correct_answer'])
+                            # Agar sub_question takrorlansa, u holda uni o'rnatmaslik
+                            if sub_tuple in seen_sub_questions:
+                                continue
+                            seen_sub_questions.add(sub_tuple)
                             sub_data['question'] = question.id  # Asosiy savolga bog'lash
                             sub_serializer = CompositeSubQuestionSerializer(data=sub_data)
                             if sub_serializer.is_valid():
