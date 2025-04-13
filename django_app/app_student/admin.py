8dcf1147ce1e4ccdf333cb646dbcb5a546662cbe
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-04-13 16:58:40 +0500

    fixed

--- django_app/app_student/admin.py
+++ django_app/app_student/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Diagnost_Student
 
+
+admin.site.register(Diagnost_Student)
 # Register your models here.
--- django_app/app_student/models.py
+++ django_app/app_student/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django_app.app_user.models import Student
 
-# Create your models here.
+
+class  Diagnost_Student(models.Model):
+    student =  models.ForeignKey(Student, on_delete=models.CASCADE)
+    result   = models.JSONField()
+
+    def __str__(self):
+        return str(self.student.full_name)
