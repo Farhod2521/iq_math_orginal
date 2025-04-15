9db224abf728a49c29cfd8a80bf95ba10a277ba3
Farhod2521 <80619246+Farhod2521@users.noreply.github.com>
2025-04-15 21:39:10 +0500

    fixed

--- django_app/app_teacher/urls.py
+++ django_app/app_teacher/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import(
      TeacherSubjectsAPIView, MyTopicAddCreateView,
        MyTopicListView, MyChapterAddCreateView, 
-       MyChapterListView, QuestionAddCreateView, MyQuestionListView
+       MyChapterListView, QuestionAddCreateView, MyQuestionListView, QuestionUpdateView, QuestionDeleteView
 )
 urlpatterns = [
     path("my-subjects/", TeacherSubjectsAPIView.as_view(), name="teacher-subjects"),
@@ -10,6 +10,8 @@ urlpatterns = [
     path('my-chapters/create/', MyChapterAddCreateView.as_view(), name='chapter-create'),
     path('my-topics/create/', MyTopicAddCreateView.as_view(), name='topic-create'),
     path('my-question/create/', QuestionAddCreateView.as_view(), name='question-create'),
+    path('my-question/update/<int:pk>/', QuestionUpdateView.as_view(), name='question-update'),
+    path('my-question/delete/<int:pk>/', QuestionDeleteView.as_view(), name='question-delete'),
     path('my-question/list/<int:id>/', MyQuestionListView.as_view(), name='question-list'),
 
     path('my-chapters/list/<int:id>/', MyChapterListView.as_view(), name='my-chapters'),
