133975368ddbd44760bd4df99ea1d3490fc998fb
Farhod2521 <80619246+Farhod2521@users.noreply.github.com>
2025-04-18 13:48:31 +0500

    fixed

--- django_app/app_user/models.py
+++ django_app/app_user/models.py
@@ -70,13 +70,15 @@ class Student(models.Model):
     brithday = models.CharField(max_length=20, verbose_name="Tug‘ilgan kun")
     academy_or_school = models.CharField(max_length=200, verbose_name="Akademiya yoki maktab")
     academy_or_school_name = models.CharField(max_length=500, verbose_name="Muassasa nomi")
+    
     class_name = models.ForeignKey(
-    'app_teacher.Subject',
-    on_delete=models.SET_NULL,
-    null=True,
-    related_name='students',
-    verbose_name="Sinf"
-)
+        'app_teacher.Subject',  # string ko'rinishida yozildi
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='students',
+        verbose_name="Sinf"
+    )
+
     document_type = models.CharField(max_length=50, verbose_name="Hujjat turi")
     document = models.CharField(max_length=20, verbose_name="Hujjat raqami")
     type_of_education = models.CharField(max_length=200, verbose_name="Ta’lim turi")
@@ -91,7 +93,6 @@ class Student(models.Model):
         verbose_name_plural = "O‘quvchilar"
 
     def get_subject_name(self):
-        from django_app.app_teacher.models import Subject
         return self.class_name.name if self.class_name else "Noma’lum"
 class Teacher(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='teacher_profile', verbose_name="Foydalanuvchi") 
