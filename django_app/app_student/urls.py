9ff22e186c823cd3a6ba83f2b5b079e7dadfb3e8
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-04-19 15:43:18 +0500

    fixed

--- django_app/app_student/urls.py
+++ django_app/app_student/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import  GenerateTestAPIView, CheckAnswersAPIView, StudentSubjectListAPIView
+from .views import (
+     GenerateTestAPIView, CheckAnswersAPIView, 
+     StudentSubjectListAPIView, ChapterListBySubjectAPIView
+)
 
 urlpatterns = [
     path('my-subjects/', StudentSubjectListAPIView.as_view(), name='my-subjects'),
+    path('my-chapter/', ChapterListBySubjectAPIView.as_view(), name='chapter-list-by-subject'),
     path('my-generate-test/', GenerateTestAPIView.as_view(), name='generate-test'),
     path('my-check-answer/', CheckAnswersAPIView.as_view(), name='my-check-answer'),
 
