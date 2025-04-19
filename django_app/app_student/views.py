ae0af63579b2e334587a0b6ad25f06cb01e85a9a
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-04-19 17:26:07 +0500

    fixed

--- django_app/app_student/views.py
+++ django_app/app_student/views.py
@@ -39,7 +39,7 @@ class GenerateTestAPIView(APIView):
 
         # Get all subjects related to the previous class for "Matematika"
         subjects = Subject.objects.filter(
-            classes_name=prev_class, category__name__iexact="Matematika"
+            classes__name=prev_class, category__name__iexact="Matematika"
         )
         if not subjects.exists():
             return Response({"message": "Matematika fani topilmadi"}, status=400)
