80286d6eec30786934d45273fb86da4ea9ee8a11
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-04-19 13:06:30 +0500

    fixed

--- config/settings/production.py
+++ config/settings/production.py
@@ -11,18 +11,14 @@ load_dotenv()
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '3306'),
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
-
 CSRF_TRUSTED_ORIGINS = [
     "https://backend.iq-math.uz",
     "https://www.backend.iq-math.uz",
