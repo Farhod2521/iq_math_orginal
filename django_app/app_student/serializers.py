b43bc6b5ceef4531634f3872c681c2b117b50317
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-04-23 10:41:32 +0500

    fixed

--- django_app/app_student/serializers.py
+++ django_app/app_student/serializers.py
@@ -11,7 +11,7 @@ class SubjectSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Subject
-        fields = ["id", "name_uz", "name_ru", "class_name", "class_uz", "class_ru"]
+        fields = ["id", "name_uz", "name_ru", "class_name", "class_uz", "class_ru", "image_uz", "image_ru"]
 
     def get_class_uz(self, obj):
         return f"{obj.classes.name}-sinf {obj.name_uz}"
