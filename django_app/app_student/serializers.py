6aec6ddda266eb3fd3c9084f7bb669e29ad11e1c
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-04-23 15:57:18 +0500

    fixed

--- django_app/app_student/serializers.py
+++ django_app/app_student/serializers.py
@@ -135,8 +135,8 @@ class CheckChoiceAnswerSerializer(serializers.Serializer):
 
 class CheckTextAnswerSerializer(serializers.Serializer):
     question_id = serializers.IntegerField()
-    answer_uz = serializers.CharField(max_length=1000)
-    answer_ru = serializers.CharField(max_length=1000)
+    answer_uz = serializers.CharField(max_length=1000, required=False)
+    answer_ru = serializers.CharField(max_length=1000, required=False)
 
 
 class CheckCompositeAnswerSerializer(serializers.Serializer):
