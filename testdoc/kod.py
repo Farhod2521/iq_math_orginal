78b356f914c91bd18e9d20ad1221523ea231a397
Farhod2521 <80619246+Farhod2521@users.noreply.github.com>
2025-04-25 00:46:24 +0500

    fixed

--- testdoc/kod.py
+++ testdoc/kod.py
@@ -1,31 +1,12 @@
-from striprtf.striprtf import rtf_to_text
+def tozalash(input_file, output_file):
+    with open(input_file, 'r', encoding='utf-8') as f:
+        content = f.read()
 
-def convert_rtf_to_html(rtf_path):
-    with open(rtf_path, 'r') as file:
-        rtf_text = file.read()
-    
-    plain_text = rtf_to_text(rtf_text)
-    
-    html_template = """
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <meta charset="UTF-8">
-        <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
-        <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
-    </head>
-    <body>
-        {content}
-    </body>
-    </html>
-    """
-    
-    # Basic conversion - you'll need to add your math processing logic here
-    content = plain_text.replace('\n', '<br>')
-    
-    return html_template.format(content=content)
+    # & belgilarini olib tashlash
+    cleaned_content = content.replace('&', '').strip()
 
-# Use this instead for RTF files
-html_output = convert_rtf_to_html("rtr.rtf")
-with open("output.html", "w", encoding="utf-8") as f:
-    f.write(html_output)
+    with open(output_file, 'w', encoding='utf-8') as f:
+        f.write(cleaned_content)
+
+# Foydalanish:
+tozalash(r'D:\iq_math_orginal\testdoc\vertopal.com_5.latex', r'D:\iq_math_orginal\testdoc\5_sinf_clean.latex')
