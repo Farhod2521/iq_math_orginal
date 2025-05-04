4f58aa4541d969e577fafbbd16c5f982d2f88a91
Farhod2521 <80619246+Farhod2521@users.noreply.github.com>
2025-05-04 14:55:54 +0500

    fixed

--- django_app/app_payments/apps.py
+++ django_app/app_payments/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AppPaymentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_payments'
+    name = 'django_app.app_payments'
