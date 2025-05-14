38a664e521245b6b9891e1110221f80373091cf9
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-05-14 10:32:01 +0500

    fixed

--- django_app/app_payments/serializers.py
+++ django_app/app_payments/serializers.py
@@ -4,4 +4,18 @@ from .models import Payment
 class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
-        fields = ['id', 'amount', 'payment_date', 'transaction_id', 'status', 'payment_gateway', 'receipt_url']
+        fields = [
+            'id',
+            'store_id',             # Multicard ID raqami
+            'student',              # Talaba
+            'invoice_uuid',         # Invoys ID raqami
+            'uuid',                 # To‘lov tranzaksiyasining ID raqami
+            'billing_id',           # Hamkor billing tizimidagi tranzaksiya ID
+            'sign',                 # MD5 HASH
+            'amount',               # To‘lov summasi
+            'payment_date',         # To‘lov sanasi
+            'transaction_id',       # Tranzaksiya ID
+            'status',               # Holat
+            'payment_gateway',      # To‘lov tizimi
+            'receipt_url'           # Chek havolasi
+        ]
