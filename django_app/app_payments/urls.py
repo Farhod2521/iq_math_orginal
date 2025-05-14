ea3dde2d8a2a08084ac997873757003b2db3434a
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-05-14 09:58:15 +0500

    fixed

--- django_app/app_payments/urls.py
+++ django_app/app_payments/urls.py
@@ -1,9 +1,13 @@
 # urls.py
 from django.urls import path
-from .views import PaymentCallbackAPIView, InitiatePaymentAPIView, SubscriptionTrialDaysAPIView
+from .views import (
+    PaymentCallbackAPIView, InitiatePaymentAPIView, 
+    SubscriptionTrialDaysAPIView, MyPaymentsAPIView
+)
 
 urlpatterns = [
     path("payment-callback/", PaymentCallbackAPIView.as_view(), name="payment-callback"),
     path("initiate-payment/", InitiatePaymentAPIView.as_view(), name="initiate-payment"),
     path("subscription/trial_days/", SubscriptionTrialDaysAPIView.as_view(), name="initiate-payment"),
+    path('my-payments/', MyPaymentsAPIView.as_view(), name='my-payments'),
 ]
