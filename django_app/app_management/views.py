1935ee04adf38ab06c54b059c8e2525dfe4f7fe0
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-05-20 10:32:23 +0500

    statisks

--- django_app/app_management/views.py
+++ django_app/app_management/views.py
@@ -1,7 +1,9 @@
 from rest_framework.generics import ListAPIView
 from .models import SystemSettings, FAQ, Product
 from .serializers import SystemSettingsSerializer, FAQSerializer, ProductSerializer
-
+from app_user.models import User, Teacher, Student
+from rest_framework.views import APIView
+from rest_framework.response import Response
 class SystemSettingsListView(ListAPIView):
     queryset = SystemSettings.objects.all()
     serializer_class = SystemSettingsSerializer
@@ -14,3 +16,19 @@ class FAQListView(ListAPIView):
 class ProductListView(ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+
+class StatisticsAPIView(APIView):
+
+    def get(self, request):
+        total_users = User.objects.count()
+        total_teachers = Teacher.objects.count()
+        total_students = Student.objects.count()
+
+        data = {
+            "total_users": total_users,
+            "total_teachers": total_teachers,
+            "total_students": total_students,
+        }
+
+        return Response(data)
