cd9d7eedab73ca361f942810576beba0d88de30c
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-06-24 21:02:09 +0500

    fixed

--- django_app/app_management/translation.py
+++ django_app/app_management/translation.py
@@ -1,8 +1,20 @@
 from modeltranslation.translator import translator, TranslationOptions
-from .models import Product
+from .models import Product,  FAQ, SystemSettings
 class ProductTranslationOptions(TranslationOptions):
     fields = ('name',)
 
 translator.register(Product, ProductTranslationOptions)
 
 
+
+class FAQTranslationOptions(TranslationOptions):
+    fields = ('question','answer')
+
+translator.register(FAQ, FAQTranslationOptions)
+
+
+
+class SystemSettingsTranslationOptions(TranslationOptions):
+    fields = ('about',)
+
+translator.register(SystemSettings, SystemSettingsTranslationOptions)
