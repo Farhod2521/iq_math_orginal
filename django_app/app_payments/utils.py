7ee07fcbb2d8f91a6f02194229ade3b319533077
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-07-16 19:51:44 +0500

    fixed

--- django_app/app_payments/utils.py
+++ django_app/app_payments/utils.py
@@ -2,10 +2,14 @@
 
 import requests
 
-MULTICARD_AUTH_URL = "https://mesh.multicard.uz/auth"
-APPLICATION_ID = "raqamli_iqtisodiyot_va_agrotexnologiyalar_universiteti"
-SECRET_KEY = "b7lydo1mu8abay9x"
+# MULTICARD_AUTH_URL = "https://mesh.multicard.uz/auth"
+# APPLICATION_ID = "raqamli_iqtisodiyot_va_agrotexnologiyalar_universiteti"
+# SECRET_KEY = "b7lydo1mu8abay9x"
 
+
+MULTICARD_AUTH_URL = "https://mesh.multicard.uz/auth"
+APPLICATION_ID = "udea"
+SECRET_KEY = "n4eci720czqjlo2t"
 def get_multicard_token():
     response = requests.post(MULTICARD_AUTH_URL, json={
         "application_id": APPLICATION_ID,
