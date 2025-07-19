269d7fc9d740669b4ff6718bb85fc179d9db568e
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-07-19 19:04:47 +0500

    fixed

--- django_app/app_student/models.py
+++ django_app/app_student/models.py
@@ -127,7 +127,7 @@ class TopicHelpRequestIndependent(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.student} - {self.subject} - {self.get_level_display()}"
+        return f"{self.student} - {self.subject}"
 
 
 
