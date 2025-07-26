9fe31e874cb37518c27437f975994e7dc7af3ae0
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-07-26 18:19:51 +0500

    fexs

--- django_app/app_student/View/independentView.py
+++ django_app/app_student/View/independentView.py
@@ -5,8 +5,8 @@ from rest_framework import status
 from django.utils.timezone import now
 from django_app.app_student.models import  TopicHelpRequestIndependent
 from django_app.app_student.serializers import  TopicHelpRequestIndependentSerializer
-
-
+from rest_framework.views import APIView
+from django.utils import timezone
 from .telegram_bot_service import send_question_to_telegram
 
 class TopicHelpRequestCreateView(CreateAPIView):
@@ -41,3 +41,49 @@ class TopicHelpRequestCreateView(CreateAPIView):
             "message": "O‘qituvchiga yuborildi"
         }, status=status.HTTP_201_CREATED)
 
+
+
+class AssignTeacherAPIView(APIView):
+    def post(self, request):
+        help_request_id = request.data.get("help_request_id")
+        telegram_id = request.data.get("telegram_id")
+
+        if not help_request_id or not telegram_id:
+            return Response({
+                "success": False,
+                "message": "help_request_id yoki telegram_id yuborilmadi."
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            help_request = TopicHelpRequestIndependent.objects.get(id=help_request_id)
+
+            # Agar allaqachon biriktirilgan bo‘lsa, boshqa o‘qituvchilarga ruxsat bermaymiz
+            if help_request.teacher:
+                return Response({
+                    "success": False,
+                    "message": f"Savolga hozirda {help_request.teacher.full_name} javob beryapti."
+                }, status=status.HTTP_403_FORBIDDEN)
+
+            teacher = Teacher.objects.get(telegram_id=telegram_id)
+
+            help_request.teacher = teacher
+            help_request.status = "reviewing"
+            help_request.reviewed_at = timezone.now()
+            help_request.save()
+
+            return Response({
+                "success": True,
+                "teacher_name": teacher.full_name
+            }, status=status.HTTP_200_OK)
+
+        except TopicHelpRequestIndependent.DoesNotExist:
+            return Response({
+                "success": False,
+                "message": "Savol topilmadi."
+            }, status=status.HTTP_404_NOT_FOUND)
+
+        except Teacher.DoesNotExist:
+            return Response({
+                "success": False,
+                "message": "O‘qituvchi topilmadi."
+            }, status=status.HTTP_404_NOT_FOUND)
--- django_app/app_user/models.py
+++ django_app/app_user/models.py
@@ -71,6 +71,7 @@ class Teacher(models.Model):
     status = models.BooleanField(default=False, verbose_name="Holat")
     is_verified_teacher = models.BooleanField(default=False, verbose_name="O‘qituvchi")
     teacher_date = models.DateTimeField(auto_now=True, null=True, verbose_name="Ro‘yxatdan o‘tgan sana")
+    telegram_id = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.full_name
