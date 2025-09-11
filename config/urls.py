3e8914d6b742669a32e18dbf395dd25265b570f5
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-09-11 16:18:29 +0500

    fixed

--- config/urls.py
+++ config/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('api/v1/func_student/', include("django_app.app_student.urls")),
     path('api/v1/management/', include("django_app.app_management.urls")),
     path('api/v1/payments/', include("django_app.app_payments.urls")),
+    path('api/v1/tutor/', include("django_app.app_tutor.urls")),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-docs'),
 ]
 
