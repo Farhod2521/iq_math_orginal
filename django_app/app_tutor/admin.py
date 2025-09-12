397b9bf75d75c0cae1df8ba380fcb7aef7fb22d1
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-09-12 20:08:03 +0500

    ficed

--- django_app/app_tutor/admin.py
+++ django_app/app_tutor/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import TutorCouponTransaction
+
+@admin.register(TutorCouponTransaction)
+class TutorCouponTransactionAdmin(admin.ModelAdmin):
+    list_display = (
+        'student', 
+        'tutor', 
+        'coupon', 
+        'payment_amount', 
+        'student_cashback_amount', 
+        'teacher_cashback_amount', 
+        'used_at'
+    )
+    list_filter = ('tutor', 'used_at', 'coupon')
+    search_fields = ('student__user__first_name', 'student__user__last_name', 'tutor__user__first_name', 'tutor__user__last_name', 'coupon__code')
+    readonly_fields = ('used_at',)
+    ordering = ('-used_at',)
