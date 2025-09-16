80bfb106942dde83558333a7bf531ef71781bfdc
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-09-16 12:17:03 +0500

    fixed

--- django_app/app_payments/admin.py
+++ django_app/app_payments/admin.py
@@ -42,11 +42,12 @@ class MonthlyPaymentAdmin(admin.ModelAdmin):
 @admin.register(SubscriptionPlan)
 class SubscriptionPlanAdmin(admin.ModelAdmin):
     list_display = (
-        'get_plan_name',     # tarif nomi (1 oylik, 3 oylik ...)
-        'price_per_month',   # oyiga narx
-        'discount_percent',  # chegirma %
-  
-        'is_active',         # faolmi
+        'get_plan_name',          # tarif nomi
+        'price_per_month',        # oyiga narx
+        'discount_percent',       # chegirma %
+        'get_discounted_price',   # chegirmali narx (oyiga)
+        'get_total_price',        # chegirmadan keyingi umumiy narx
+        'is_active',
         'created_at',
     )
     list_filter = ('is_active', 'months',)
@@ -65,6 +66,17 @@ class SubscriptionPlanAdmin(admin.ModelAdmin):
     def get_plan_name(self, obj):
         """Tarif nomi (oylar)"""
         return obj.get_months_display()
-    get_plan_name.short_description = "Tarif nomi (oylar)"  # Admin ustun nomi
+    get_plan_name.short_description = "Tarif nomi (oylar)"
+
+    def get_discounted_price(self, obj):
+        """Chegirmali narx (oyiga)"""
+        discount_amount = (obj.price_per_month * obj.discount_percent) / 100
+        return obj.price_per_month - discount_amount
+    get_discounted_price.short_description = "Chegirmali narx (oyiga)"
+
+    def get_total_price(self, obj):
+        """Chegirmadan keyingi umumiy narx"""
+        return obj.total_price()
+    get_total_price.short_description = "Umumiy narx (chegirmadan keyin)"
 
  
