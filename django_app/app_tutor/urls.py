ece8c44e5181551e622fe1632211644bda89b15b
Farhod2521 <80619246+Farhod2521@users.noreply.github.com>
2025-09-27 00:58:47 +0500

    fixed

--- django_app/app_tutor/urls.py
+++ django_app/app_tutor/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TutorCreateCouponAPIView
+from .views import TutorCreateCouponAPIView, TutorCreateReferralAPIView
 
 urlpatterns = [
     path('tutor/create-coupon/', TutorCreateCouponAPIView.as_view(), name='tutor-create-coupon'),
+    path('tutor/create-referral/', TutorCreateReferralAPIView.as_view(), name='tutor-create-coupon'),
 ]
