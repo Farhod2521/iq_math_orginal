f5b871b1230a919716cd5ca9ea59bc24846dc421
Farhod2521 <80619246+Farhod2521@users.noreply.github.com>
2025-09-27 01:46:00 +0500

    fixed

--- django_app/app_tutor/urls.py
+++ django_app/app_tutor/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
-from .views import TutorCreateCouponAPIView, TutorCreateReferralAPIView
+from .views import  TutorCouponTransactionListAPIView, TutorReferralTransactionListAPIView, TutorCouponViewSet, TutorReferralViewSet
+
+
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'tutor/coupons', TutorCouponViewSet, basename='tutor-coupon')
+router.register(r'tutor/referrals', TutorReferralViewSet, basename='tutor-referral')
+
 
 urlpatterns = [
-    path('tutor/create-coupon/', TutorCreateCouponAPIView.as_view(), name='tutor-create-coupon'),
-    path('tutor/create-referral/', TutorCreateReferralAPIView.as_view(), name='tutor-create-coupon'),
-]
+    path('tutor/coupon-transactions/', TutorCouponTransactionListAPIView.as_view(), name='tutor-coupon-transactions'),
+    path('tutor/referral-transactions/', TutorReferralTransactionListAPIView.as_view(), name='tutor-referral-transactions'),
+]+ router.urls
