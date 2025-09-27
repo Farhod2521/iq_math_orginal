5d627cfbba5ca9f7dd486b2ddb3de131bf7e6408
Farhod2521 <80619246+Farhod2521@users.noreply.github.com>
2025-09-28 03:19:58 +0500

    fixed

--- django_app/app_management/urls.py
+++ django_app/app_management/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import SystemSettingsListView, FAQListView, ProductListView, StatisticsAPIView, BannerListView
+from .views import SystemSettingsListView, FAQListView, ProductListView, FullStatisticsAPIView, BannerListView
 
 urlpatterns = [
     path('system-settings/', SystemSettingsListView.as_view(), name='system-settings-list'),
     path('banner/', BannerListView.as_view(), name='system-settings-list'),
     path('faqs/', FAQListView.as_view(), name='faq-list'),
     path('products/', ProductListView.as_view(), name='product-list'),
-    path('statistics/', StatisticsAPIView.as_view(), name='statistics'),
+    path('statistics/', FullStatisticsAPIView.as_view(), name='statistics'),
 ]
