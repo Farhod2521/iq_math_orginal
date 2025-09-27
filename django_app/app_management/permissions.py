5b46bde2360646351c552ea8013be2bb6fd3575b
Farhod2521 <80619246+Farhod2521@users.noreply.github.com>
2025-09-28 03:22:36 +0500

    fixed

---
+++ django_app/app_management/permissions.py
@@ -0,0 +1,9 @@
+from rest_framework.permissions import BasePermission
+
+class IsTeacher(BasePermission):
+    """
+    Faqat Teacher rolidagi foydalanuvchilar ko‘ra oladi
+    """
+    def has_permission(self, request, view):
+        # Agar sizda User modelida role degan field bo‘lsa:
+        return bool(request.user and request.user.is_authenticated and request.user.role == 'teacher')
