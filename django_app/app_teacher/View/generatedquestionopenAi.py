53e109fb1d36f1cce11ef71d737ee5446bba8437
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-10-25 13:47:56 +0500

    fixed

--- django_app/app_teacher/View/generatedquestionopenAi.py
+++ django_app/app_teacher/View/generatedquestionopenAi.py
@@ -1,15 +1,19 @@
-from openai import OpenAI
-import random
-from django_app.app_teacher.models import Topic, Question, GeneratedQuestionOpenAi
-import os
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-import threading
 from .generate_topic_questions_script import generate_topic_questions
 
 class GenerateAIQuestionsAPIView(APIView):
     def post(self, request):
+        subject_id = request.data.get("subject_id")
+        chapter_id = request.data.get("chapter_id")
         topic_id = request.data.get("topic_id")
-        threading.Thread(target=generate_topic_questions, args=(topic_id,)).start()
-        return Response({"success": True, "message": "Jarayon boshlandi (AI generatsiya fon rejimida)."})
+
+        if not (subject_id and chapter_id and topic_id):
+            return Response({"error": "subject_id, chapter_id va topic_id kiritilishi kerak"}, status=400)
+
+        try:
+            count = generate_topic_questions(subject_id, chapter_id, topic_id)
+            return Response({"success": True, "generated_count": count}, status=200)
+        except Exception as e:
+            return Response({"error": str(e)}, status=500)
