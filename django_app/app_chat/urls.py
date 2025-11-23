0db7ecea4c38efa50c8a234bb36af70d57ef7802
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-11-23 12:47:02 +0500

    fixed

---
+++ django_app/app_chat/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .views import (
+    CreateDirectChatAPIView,
+    SendMessageAPIView,
+    ReadMessageAPIView
+)
+
+urlpatterns = [
+    # 1) Student ↔ Teacher o‘rtasida direct chat yaratish
+    path("chat/create-direct/", CreateDirectChatAPIView.as_view(), name="create-direct-chat"),
+
+    # 2) Chatga xabar yuborish
+    path("chat/<int:conversation_id>/send/", SendMessageAPIView.as_view(), name="chat-send-message"),
+
+    # 3) Xabarni o‘qilgan deb belgilash
+    path("chat/message/<int:message_id>/read/", ReadMessageAPIView.as_view(), name="read-message"),
+]
