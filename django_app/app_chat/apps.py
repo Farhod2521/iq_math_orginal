86855655d8f6e0d2700becf72a52906e02a25e3d
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-11-23 12:44:05 +0500

    fixed

--- django_app/app_chat/apps.py
+++ django_app/app_chat/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AppChatConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_chat'
+    name = 'django_app.app_chat'
