3bd014d68099e9e7c62ef14bc25f2bff7e0a969b
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-12-29 10:13:25 +0500

    fixed

--- django_app/app_management/models.py
+++ django_app/app_management/models.py
@@ -23,12 +23,11 @@ class AndroidVersion(models.Model):
     ios_force_update = models.BooleanField(default=False)
 
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return "Mobile App Versions"
     
-    
+
 class UploadSetting(models.Model):
     max_size_mb = models.PositiveIntegerField(
         default=5,
