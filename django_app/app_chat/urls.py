2ef8f7be9124860cee1063ad49419c5a4ecc9205
Farhod2521 <abdikarimovfarhod86@gmail.com>
2026-01-12 12:53:23 +0500

    fixed

--- django_app/app_chat/urls.py
+++ django_app/app_chat/urls.py
@@ -4,7 +4,7 @@ from .views import (
     SendMessageAPIView,
     ReadMessageAPIView,
     UniversalChatsAPIView, RateConversationAPIView,
-    ConversationMessagesAPIView,  CloseConversationAPIView
+    ConversationMessagesAPIView,  CloseConversationAPIView, TotalUnreadChatsAPIView
 )
 
 urlpatterns = [
@@ -19,4 +19,5 @@ urlpatterns = [
     path("chat/message/<int:message_id>/read/", ReadMessageAPIView.as_view()),
     path("conversations/<int:conversation_id>/close/", CloseConversationAPIView.as_view(), name="close-conversation"),
     path("conversations/<int:conversation_id>/rate/", RateConversationAPIView.as_view(), name="rate-conversation"),
+    path("chats/unread-total/",TotalUnreadChatsAPIView.as_view(),name="total-unread-chats"),
 ]
