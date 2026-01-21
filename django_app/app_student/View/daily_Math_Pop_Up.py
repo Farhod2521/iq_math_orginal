73914ebc3f17c88fd9df21a320d60ccc78a28e63
Farhod2521 <abdikarimovfarhod86@gmail.com>
2026-01-21 09:57:38 +0500

    fixed

--- django_app/app_student/View/daily_Math_Pop_Up.py
+++ django_app/app_student/View/daily_Math_Pop_Up.py
@@ -4,7 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from django_app.app_student.models import TopicProgress
-from django_app.app_teacher.models import Question
+from django_app.app_teacher.models import Question, Topic
 
 from django_app.app_user.models import Student
 
@@ -23,19 +23,41 @@ class QuickMathQuestionAPIView(APIView):
                 status=404
             )
 
+        # 1️⃣ Oldin ishlangan mavzularni tekshiramiz
         topic_ids = TopicProgress.objects.filter(
             user=student,
             score__gt=0
         ).values_list("topic_id", flat=True)
 
-        if not topic_ids:
-            return Response(
-                {"detail": "Score 60 dan yuqori mavzular topilmadi"},
-                status=404
+        # 2️⃣ Agar ishlangan mavzular bo‘lsa
+        if topic_ids.exists():
+            topic_id = random.choice(list(topic_ids))
+            topics = Topic.objects.filter(id=topic_id)
+
+        # 3️⃣ Aks holda: student tanlagan fanidan olamiz
+        else:
+            subject = student.class_name  # bu Subject
+
+            if not subject:
+                return Response(
+                    {"detail": "Student fan tanlamagan"},
+                    status=404
+                )
+
+            topics = Topic.objects.filter(
+                chapter__subject=subject
             )
 
-        topic_id = random.choice(list(topic_ids))
+            if not topics.exists():
+                return Response(
+                    {"detail": "Bu fan uchun mavzular topilmadi"},
+                    status=404
+                )
 
+            topic = random.choice(list(topics))
+            topic_id = topic.id
+
+        # 4️⃣ O‘sha topic bo‘yicha savollar
         questions = Question.objects.filter(
             topic_id=topic_id,
             question_type="text"
@@ -56,6 +78,7 @@ class QuickMathQuestionAPIView(APIView):
             "question_text": question.question_text,
             "level": question.level
         })
+
 
 
 
