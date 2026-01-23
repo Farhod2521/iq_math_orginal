f1753741fc195563ab070c2b79337217333f2153
Farhod2521 <abdikarimovfarhod86@gmail.com>
2026-01-23 10:40:58 +0500

    fixed

--- config/asgi.py
+++ config/asgi.py
@@ -1,11 +1,13 @@
 import os
+
+# 🔥 ENG MUHIM QATOR — ENG BIRINCHI
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
-from django_app.app_chat import routing  # 🔥 MUHIM
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+from django_app.app_chat import routing
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
