d1407ff257588ee2601cd93578340f75aa31e8aa
Farhod2521 <abdikarimovfarhod86@gmail.com>
2026-01-23 19:09:36 +0500

    fixed

--- config/routing.py
+++ config/routing.py
@@ -1,12 +1,13 @@
 import os
 
-import django
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-
 os.environ["DJANGO_SETTINGS_MODULE"] = "config.settings.production"
+
+import django
+
 django.setup()
 
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
 from django_app.app_chat import routing as chat_routing
 from django_app.app_chat.middleware import JwtAuthMiddlewareStack
 
